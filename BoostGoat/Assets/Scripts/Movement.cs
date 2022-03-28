81162898b0df1459dc2875f953df7d4d07376f65
RyanXCG <xiayu1994129@gmail.com>
2022-03-28 01:17:51 -0400

    added thrust

--- BoostGoat/Assets/Scripts/Movement.cs
+++ BoostGoat/Assets/Scripts/Movement.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+  Rigidbody rb;
+
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    ProcessInput();
+  }
+
+  void ProcessInput()
+  {
+    if (Input.GetKey(KeyCode.Space))
     {
-        
+      //Debug.Log("pressed thrusting");
+      rb.AddRelativeForce(0, 1, 0);
+    }
+    // left rotate is given priority
+    if (Input.GetKey(KeyCode.A))
+    {
+      Debug.Log("pressed left Rotate");
+    }
+    else if (Input.GetKey(KeyCode.D))
+    {
+      Debug.Log("pressed right Rotate");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  }
 }
